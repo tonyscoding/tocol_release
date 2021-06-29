02d0036c43c8c2af027b5871c1fae3c05d5b3beb
hansolc <hsc080808@naver.com>
2021-06-29 11:55:24 +0900

    Setting for mac env

---
+++ notarize.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+exports.default = async function notarizing(context) {
+    const { electronPlatformName, appOutDir } = context;
+    if(electronPlatformName !== 'darwin') return;
+
+    const appName = context.packager.appInfo.productFileName;
+    return await notarize({
+        appBundleId: 'kr.co.topolar.topolar',
+        appPath: `${appOutDir}/${appName}.app`,
+        appleId: 'hansol.choi@tonyscoding.com',
+        appleIdPassword: 'sxkv-xyqm-wzwd-hxcp'
+    })
+}
