b6ba066dd94d16ec5f20835316dd7407ed6dfde4
최한솔 <choehansol@choehansol-ui-MacBookAir.local>
2021-07-01 11:19:36 +0900

    electron release file updated

--- notarize.js
+++ notarize.js
@@ -4,11 +4,11 @@ exports.default = async function notarizing(context) {
     const { electronPlatformName, appOutDir } = context;
     if(electronPlatformName !== 'darwin') return;
 
-    const appName = context.packager.appInfo.productFileName;
+    const appName = context.packager.appInfo.productFilename;
     return await notarize({
         appBundleId: 'kr.co.topolar.topolar',
         appPath: `${appOutDir}/${appName}.app`,
         appleId: 'hansol.choi@tonyscoding.com',
-        appleIdPassword: 'sxkv-xyqm-wzwd-hxcp'
+        appleIdPassword: 'smtu-illr-pisu-luvw'
     })
 }
